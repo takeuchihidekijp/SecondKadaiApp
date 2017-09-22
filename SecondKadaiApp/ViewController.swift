890d38b683368a672a619ec697f66aa44e6e841a
takeuchihidekijp <takeuchihidekijp@yahoo.co.jp>
2017-09-23 04:20:47 +0900

    課題前

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    @IBAction func unwind(_ segue: UIStoryboardSegue){
+        // 他の画面から segue を使って戻ってきた時に呼ばれる
+        
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +25,12 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // segueから遷移先のResultViewControllerを取得する
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        
     }
 
 
